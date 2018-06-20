77cc8bb12cfb39b916c8ff9c7fb9485b859bf7cf
Ross Mauck <ross@rossmauck.com>
2018-06-19 19:56:06 -0700

    Update cmc-with-words.py
    Added error catches and second try logic

--- python/cmc-with-words.py
+++ python/cmc-with-words.py
@@ -3,7 +3,6 @@ import inflect
 # Read ticker information from coinmarketcap and function for parsing
 # information by specific coins using their coin index (i.e. Ethereum = 1027)
 
-user_coin_choice = raw_input("What coin would you like info for?")
 # Create number to word engine
 p = inflect.engine()
 
@@ -11,10 +10,8 @@ url = "https://api.coinmarketcap.com/v2/ticker/"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
 
-# Set up coin interface index json functionality
-json_coin_index_data = open("coin_ids.json").read()
-coin_index_data = json.loads(json_coin_index_data)
-
+#print(coinkeys)
+#print(enumerate(coinkeys))
 # Read ticker information from loaded json data and print specific coins'
 #  current information to screen
 def getticker(n):
@@ -39,16 +36,32 @@ def getticker(n):
     print("Symbol: " + tickersymbol)
     print("Rank: " + p.number_to_words(tickerrank))
     print("Price: " + p.number_to_words( int(tickerUSDprice) ) )
-    print("Market Cap: " + p.number_to_words(int(tickerUSDmarketcap)))
-    print("24h Volume: " + p.number_to_words(int(tickerUSDvolume24h)))
+    print("Market Cap: " + p.number_to_words(int(round(tickerUSDmarketcap,4))))
+    print("24h Volume: " + p.number_to_words(int(round(tickerUSDvolume24h,4))))
     print("Price Change 1h: " + str(tickerUSDpercentchange1h) )
     print("Price Change 24h: " + str(tickerUSDpercentchange24h) )
     print("Price Change 7d: " + str(tickerUSDpercentchange7d) )
-    print("Total Supply: " + p.number_to_words(int(tickertotalsupply)) )
-    print("Circulating Supply: " + p.number_to_words(int(tickercirculatingsupply)) )
+    print("Total Supply: " + p.number_to_words(int(round(tickertotalsupply,4))) )
+    print("Circulating Supply: " + p.number_to_words(int(tickercirculatingsupply) ))
     if tickermaxsupply is None:
-        print("Max Supply: " + p.number_to_words(tickermaxsupply) )
+        print("Max Supply: " + "None")
     else:
         print("Max Supply: " + p.number_to_words(int(tickermaxsupply)) )
 
-getticker(coin_index_data[user_coin_choice])
+
+# Set up coin interface index json functionality
+json_coin_index_data = open("coin_ids.json").read()
+coin_index_data = json.loads(json_coin_index_data)
+
+user_coin_choice = raw_input("What coin would you like info for?: ")
+coinkeys = coin_index_data.keys()
+
+if user_coin_choice in coinkeys:
+    getticker(coin_index_data[user_coin_choice])
+else:
+    print("That is not a valid choice. Please use full name with caps. (i.e. Ethereum)\nPlease Try again.\n")
+    user_coin_choice = raw_input("What coin would you like info for?: ")
+    if user_coin_choice in coinkeys:
+        getticker(coin_index_data[user_coin_choice])
+    else:
+        print("I'm sorry that is still not an accepable coin name")
